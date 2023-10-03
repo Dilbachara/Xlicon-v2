288ba658aee05766980573685ea12c6c8ccce6fd
Dilbachara <113753473+Dilbachara@users.noreply.github.com>
2023-10-03 08:20:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
-global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.Owner = ["༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫;         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["917059102517"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["917059102517"];
+global.OwnerName = "༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫;
+global.BotName = "XLion sagar";
+global.packname = "༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫";                             //Do not change.
+global.author = "By ༄℣ɪᴘ᭄░S░A░G░A░R࿐♥️✨💫";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
 
@@ -33,12 +33,12 @@ global.sessionName = "session";                          //Do not change.
 
 
 //
-global.prefa = ["."];                                    //Default prefix here.
+global.prefa = ["*"];                                    //Default prefix here.
 global.openAiAPI = "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";
 
 
 //
-global.location = "Dhaka, Bangladesh";                   
+global.location = "kolkata, barasat";                   
 global.reactmoji = "✅";
 global.themeemoji = "💖";
 global.vidmenu = { url: 'https://tenor.com/view/jujutsu-kaisen0-yuta-okkotsu-gif-26767662' };
@@ -80,7 +80,7 @@ global.mess = {
     useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
     botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
     botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
+    grouponly: 'This command is only made for *Groups*,fucking Baka!',
     privateonly: 'This command is only made for *Private Chat*, Baka!',
     botonly: 'Only the *Bot itself* can use this command!',
     waiting: 'Just Wait...',
